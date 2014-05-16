add2ddc956534a0b209ef9629ae6ffc340ba828c
mikalai-ramashka <nikolay.romashko@gmail.com>
2014-05-16 17:51:45 +0300

    - fixed tests

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -1,11 +1,11 @@
-﻿define(['jquery', 'underscore', 'views/task'], function ($, _, TaskView) {
+﻿define(['jquery', 'underscore', 'views/app'], function ($, _, AppView) {
 
     describe('checking dependency', function () {
         'use strict';
 
         it('works for application', function () {
 
-            var app = new TaskView();
+            var app = new AppView($("<div/>"));
             app.render();
         });
     });
